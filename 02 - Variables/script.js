febbac300d7231950443464d1530c94bdc470245
Lehuan07 <nahuelncejas@gmail.com>
2025-07-04 14:56:54 -0300

    Variables, primeros pasos

---
+++ 02 - Variables/script.js
@@ -0,0 +1,20 @@
+
+
+//Variables:
+//Es una caja donde guardo una informacion:
+
+//var nombre = "Nahuel";
+let heroe = "Mi papá";
+let universo = "Sistema solar";
+let edad = 72;
+
+let heroeUniversal = heroe + " Es un super heroe de la familia " + universo;
+
+heroe = "Spiderman";
+universo = "Marvel";
+edad = 25;
+console.log(heroe + " " + universo + " " + edad);
+
+alert(heroeUniversal);
+
+//const nacionalidad = "Argentino";
