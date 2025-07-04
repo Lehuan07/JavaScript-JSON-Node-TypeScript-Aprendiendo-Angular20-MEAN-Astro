1db330b97edd5cc21930659d2adf28ebd913bc04
Nahuel C. <nahuelncejas@gmail.com>
2025-07-04 19:19:08 -0300

    Tipos de datos

--- 04-TiposDeDatos/script.js
+++ 04-TiposDeDatos/script.js
@@ -1,2 +1,23 @@
 
 
+let frase = "Hola, soy lehuanosky"; //String o cadena
+
+let anio = 2024; //Tipo de dato entero.
+
+let interes = 2.7 //Numero decimal - float - double
+
+let mayorEdad = true //booleano.
+
+let vacia; //Valor underfined
+let nula = null //Variable nula
+
+let frutas = ["Fresa", "Sandia", "Naranja"]; //Array
+
+let Heroe = {
+    nombre: "Batman",
+    universo: "DC"
+}; //Objeto.
+
+
+console.log(frase, interes, mayorEdad);
+
