f7bbf2344d97646fe2ab8965c39ab6300546226f
Lehuan07 <nahuelncejas@gmail.com>
2025-07-04 14:42:14 -0300

    Primeros pasos - Alertas, console.log y comentarios
    Alertas, console.log y comentarios

---
+++ 01 - Primer programa con JavaScript/main.js
@@ -0,0 +1,12 @@
+/*
+alert("Hola, que tal? Soy un mensaje. A meterle pilas")
+alert("Estoy aprendiendo JS");
+*/
+
+//Estos mensajes aparece en consola:
+
+console.log("JavaScript es uno de los lenguajes mas demandados")
+
+console.log("Soy un mensaje enviado desde JS")
+
+console.log(123+12);
