00a698d7a8c6abdbd4e7323b96fa2a4b4bf0a724
Nahuel C. <nahuelncejas@gmail.com>
2025-07-04 21:50:59 -0300

    Operadores

---
+++ 05-Operadores/main.js
@@ -0,0 +1,80 @@
+
+
+//Operadores matematicos.
+let numero1 = 12;
+let numero2 = 44;
+let suma = numero1 + numero2;
+console.log("Suma: " + suma);
+
+let resta = numero1 - numero2;
+console.log("Resta: " + resta);
+
+let multiplicacion = numero1*numero2;
+console.log("Multiplicacion: " + multiplicacion);
+
+let division = numero1/numero2;
+console.log("Division: " + division);
+
+let resto = numero1%numero2;
+console.log("Resto: " + resto);
+
+let potencia = numero1**2;
+console.log("Potencia: " + potencia);
+
+//Operadores Asignacion
+let numerito = 17;
+numerito *= 3;
+console.log("Numerito: " + numerito);
+
+//Operadores Comparacion
+
+let numerazo = 31;
+
+console.log("Esto es verdad? " + numerazo === "31");
+
+console.log("Este numero es desigual? " + numerazo!= 31);
+console.log("Este numero es desigual? " + numerazo!== "31");
+
+console.log("Es mayor que " + numerazo >34);
+console.log("Es menor o igual " + numerazo <= 20);
+
+//Operadores Logicos
+
+let EdadMayor = false;
+let tieneEntrada = true;
+
+console.log(EdadMayor&&tieneEntrada);
+
+console.log(EdadMayor||tieneEntrada);
+
+console.log(!EdadMayor);
+
+//Operadores cadenas.
+
+let mensaje1 = "Hola";
+let mensaje2 = "Que tal? "
+let mensajeTotal = mensaje1 + " " + mensaje2;
+mensajeTotal+= "Soy Nahuelardo"
+
+console.log(mensajeTotal);
+
+//Operadores incrementos o decrementos.
+
+let cifra = 1200;
+cifra++;
+cifra++;
+cifra++;
+cifra++;
+cifra++;
+cifra++;
+cifra++;
+cifra++;
+cifra++;
+cifra++;
+console.log(cifra);
+cifra--;
+cifra--;
+cifra--;
+cifra--;
+cifra--;
+console.log(cifra);
