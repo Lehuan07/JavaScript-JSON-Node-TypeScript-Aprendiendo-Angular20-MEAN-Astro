13e0d30a1b7696bdba5b4b8246682592ab803f0f
Nahuel C. <nahuelncejas@gmail.com>
2025-07-05 09:43:03 -0300

    Bucle for

---
+++ 09-Bucles/script.js
@@ -0,0 +1,12 @@
+/*
+Bucle for:
+Es una estructura de control que ejecuta un bloque de codigo 
+varias veces de forma automatica mientras se cumpla la condicon*/
+
+//Ejemplo
+
+let numero = 177;
+
+for(let i = 0; i <= numero; i++){
+    console.log(i)
+}
