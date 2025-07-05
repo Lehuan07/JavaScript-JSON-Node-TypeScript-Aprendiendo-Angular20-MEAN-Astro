c70c85bcb125fa7c03e9421659188a289088407d
Nahuel C. <nahuelncejas@gmail.com>
2025-07-05 09:58:28 -0300

    Ventanas pop en JS

---
+++ 11-Ventanas/script.js
@@ -0,0 +1,7 @@
+
+//Alertas
+let mensaje = alert("comiste?");
+//Ventanas de confirmacion
+let confirmacion = confirm("Estas muy muy seguro?")
+//Prompt
+let introducilo = prompt("Cual es el nombre de tu perro salchicha?")
