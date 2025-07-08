fb424f649412639bb39660587ecb0fc811c1b7b2
Nahuel C. <nahuelncejas@gmail.com>
2025-07-08 10:35:45 -0300

    Ejercicio 23 y ultimo

---
+++ 12-Ejercicios/ejercicio23.js
@@ -0,0 +1,10 @@
+/*
+Tenemos que despegar un cohete al espacio.
+Tenemos que hacer una cuenta regresiva de 10 a 0
+Hacelo con un for*/
+
+for(i=10; i>=1; i--){
+    console.log(i)
+}
+
+ console.log("DESPEGAMOS!")
