4cb6eff279d156d99cc5b4a81915a15b772efef6
Nahuel C. <nahuelncejas@gmail.com>
2025-07-08 10:30:02 -0300

    Ejercicio 22

---
+++ 12-Ejercicios/ejercicio22.js
@@ -0,0 +1,19 @@
+
+/*Ejercicio 22:
+Tenemos una bolsa con 37 caramelos.
+cada vez que te comes uno, quedan menos.
+Hace una simulacion de esto con un bucle.
+*/
+
+let caramelos = 37;
+
+
+
+while(caramelos>0){
+    console.log("Te comiste un caramelo, te quedan " +caramelos+ " en la bolsa")
+    caramelos--;
+
+}
+
+
+
