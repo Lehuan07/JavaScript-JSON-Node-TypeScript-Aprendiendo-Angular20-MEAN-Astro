af5eeb4ef784c8c596f428cb8e14a949ee51ae9e
Nahuel C. <nahuelncejas@gmail.com>
2025-07-10 10:34:31 -0300

    Ejercicio 25 realizado
    /*
    Ejercicio 25:
    
    Crea un array con ingredientes para una receta absurda
    (Ejemplo pizza con chocolate y anana)
    
    Muestra ese array en una frase divertida
    */

---
+++ 19-ejercicios/ejercicio25.js
@@ -0,0 +1,17 @@
+/*
+Ejercicio 25:
+
+Crea un array con ingredientes para una receta absurda
+(Ejemplo pizza con chocolate y anana)
+
+Muestra ese array en una frase divertida
+*/
+
+let ingredientes = ["Dientes de alf", "Peperoni", "Ojos de bruja", "Colmillo de murcielago", "300g de chocolate", "Piedra de muralla medieval rallada"];
+
+alert(`🧙‍♂️ Receta Secreta:
+
+Necesitás:
+- ${ingredientes.join("\n- ")}
+
+¿Estás listo para probar esta *delicia mágica*? 🍕✨`);
