0346ccb2400913ac8081eefb4cccc52ec2f1f818
Nahuel C. <nahuelncejas@gmail.com>
2025-07-10 01:13:16 -0300

    Arrays multidimensionales

--- 17-ArraysAvanzados/script.js
+++ 17-ArraysAvanzados/script.js
@@ -78,3 +78,78 @@ let dondeViven = actores.map(actor => {
 
 document.write(dondeViven);
 
+//Arrays multidimencionales
+
+let categorias =["Estrategia", "Accion", "Deportes", "Surviver", "Aventuras"]
+let titulos = ["Age of empires", "Call of duty", "Fifa", "Project Zomboid", "Zelda"];
+
+let videojuegos = [categorias, titulos];
+console.log(videojuegos)
+console.log(videojuegos[1][4])
+console.log(videojuegos[0][2])
+
+//Arrays bidimensionales
+
+let tabla = [
+    ["Producto", "Cantidad", "Precio"],
+    ["PC",            2,         500 ],
+    ["Smartphone",    4,        200  ],
+    ["Impresora",     10,       57   ]
+];
+
+console.table(tabla)
+//console.log(tabla[2][0], tabla[2][2]);
+
+document.write("<table border=1>")
+
+tabla.forEach((fila, index) =>{
+
+    document.write("<tr>");
+
+    fila.forEach(columna=>{
+        if(index===0){
+            document.write("<th>" + columna + "</th>") 
+        }else{
+            document.write("<td>" + columna + "</td>") 
+        }
+        
+    })
+    document.write("</tr>")
+})
+
+document.write("</table>")
+
+console.log("********************************")
+//Arrays Tridimencional
+
+let cartaRestaurante = [
+    ["Plato", "Cantidad",    "Precio",              "Tamaño"],
+    ["Pizza",     2,       [8, 10, 15], ["Pequeño", "Mediano", "Grande"]],
+    ["Burger",    4,       [10, 12, 17], ["Pequeño", "Mediano", "Grande"]],
+    ["Asado",     10,      [14, 18, 25], ["Pequeño", "Mediano", "Grande"]]  
+];
+
+for(let menu of cartaRestaurante){
+    
+    console.log("********************************")
+
+    console.log(menu)
+
+    if(Array.isArray(menu[2] && Array.isArray(menu[3]))){
+        console.log(menu[0]);
+        for(let index in menu[2]){
+            console.log(menu[3][index], menu[2][index] + "Pesos")
+        }
+    }
+    
+    /*for(elemento of menu){
+        
+        if(Array.isArray(elemento)){
+            for(let subElemento of elemento){
+                console.log(subElemento);
+            }
+        }else{
+            console.log(elemento)
+        }
+    }*/
+}
