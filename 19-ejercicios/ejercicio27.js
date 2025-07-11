21b8abe137af2a775fc45c7cd5b02f2d386fd53c
Lehuan07 <nahuelncejas@gmail.com>
2025-07-10 23:16:09 -0300

    Ejercicio 27
    Ejercicio 27:
    
    Crea un array de personajes de peliculas y
    clasificalos en grupos (heroes, villanos etc.)
    
    Muestra un mensaje para cada grupo.

---
+++ 19-ejercicios/ejercicio27.js
@@ -0,0 +1,20 @@
+/*
+Ejercicio 26:
+
+Crea un array de personajes de peliculas y
+clasificalos en grupos (heroes, villanos etc.)
+
+Muestra un mensaje para cada grupo.
+
+*/
+
+let personajes = ["M:Joker", "B:Batman", "B:Superman", "M:Tor", "B:Harry Potter", "M:Voldemort"];
+
+let heroes =personajes.filter(personaje => personaje.startsWith("B:"));
+let villanos = personajes.filter(personaje => personaje.startsWith("M:"));
+
+let nombresHeroes = heroes.map(nombre => nombre.slice(2));
+let nombresVillanos = villanos.map(nombre => nombre.slice(2));
+
+document.write(`Los heroes son: ${nombresHeroes.join(", ")}<br>
+Los villanos son: ${nombresVillanos.join(", ")}`);
