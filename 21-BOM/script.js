a3d9a5a95621487bb983542cc6ca4383f3c8e8a7
Lehuan07 <nahuelncejas@gmail.com>
2025-07-11 12:49:29 -0300

    Todo (o casi) sobre el BOM

--- 21-BOM/script.js
+++ 21-BOM/script.js
@@ -23,3 +23,42 @@ console.log(window.closed);
 console.log(window.document)
 console.log(window.navigator.userAgent)
 console.log(window.scrollX, window.scrollY)
+
+//Navigator
+console.log(navigator.appName, navigator.appVersion)
+
+if(!navigator.onLine){
+    alert("No tenes internet macho")
+}
+else(
+    console.log("Estas en linea")
+)
+
+//Location
+
+console.log(location.href)
+console.log(location.hostname);
+
+//location.href="https://google.com"
+
+//location.reload;
+
+//Historial
+
+history.back();
+history.forward();
+
+history.go(7);
+console.log(history.length);
+
+
+//Screen
+
+console.log(screen.width, screen.height);
+
+//Ventanas emergentes o PopUps
+
+let miweb = window.open("https://google.com", 
+                        "Ir a google",
+                        "width = 750, heigth= 300",
+                        "left=552, top= 50");
