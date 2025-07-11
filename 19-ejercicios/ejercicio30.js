3bbc20cd8437df27615ea2bf4f386c3ba4527b3d
Nahuel C. <nahuelncejas@gmail.com>
2025-07-11 00:58:36 -0300

    Ejercicio 30:
        Crea una funcion que reciba un numero
        y devuelva su factorial.

---
+++ 19-ejercicios/ejercicio30.js
@@ -0,0 +1,24 @@
+/*
+Ejercicio 30:
+    Crea una funcion que reciba un numero
+    y devuelva su factorial.
+*/
+
+function calcularFactorial(numero) {
+    if (numero < 0) {
+        return "El factorial no está definido para números negativos.";
+    }
+    if (numero === 0 || numero === 1) {
+        return 1;
+    }
+    let factorial = 1;
+    for (let i = 2; i <= numero; i++) {
+        factorial *= i;
+    }
+    return factorial;
+}
+let numero = parseInt(prompt("Ingrese un número para calcular su factorial:"));
+if (!isNaN(numero)) {
+    let resultado = calcularFactorial(numero);
+    alert(`El factorial de ${numero} es ${resultado}.`);
+}
