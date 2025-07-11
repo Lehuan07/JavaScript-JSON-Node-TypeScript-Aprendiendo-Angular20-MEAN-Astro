72f0cdbd85c3974d3523f840dbdcfe5f975e7db6
Nahuel C. <nahuelncejas@gmail.com>
2025-07-11 10:44:52 -0300

    BOM primera parte

---
+++ 21-BOM/script.js
@@ -0,0 +1,25 @@
+
+//BOM browser object model
+
+
+//Alertas y dialogos
+/*
+alert("Hola abuela, desde donde estes")
+
+prompt("Como se llama la abuelosky?")
+
+confirm("Tu abuela tiene mas de 70 años?")
+*/
+
+//Objeto Window
+
+console.log(window.innerWidth);
+console.log(window.innerHeight);
+
+console.log(window.outerHeight);
+console.log(window.outerHeight);
+
+console.log(window.closed);
+console.log(window.document)
+console.log(window.navigator.userAgent)
+console.log(window.scrollX, window.scrollY)
