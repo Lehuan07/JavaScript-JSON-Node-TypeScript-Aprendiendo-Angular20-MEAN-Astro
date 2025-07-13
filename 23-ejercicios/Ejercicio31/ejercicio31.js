9b5364e59888e80fc3d90d883b9bd02da63d9848
Nahuel C. <nahuelncejas@gmail.com>
2025-07-13 02:08:57 -0300

    HTML y CSS del ejercicio 31 echo

---
+++ 23-ejercicios/Ejercicio31/ejercicio31.js
@@ -0,0 +1,15 @@
+/*
+Ejercicio 31:
+
+Crea un semáforo con tres luces (rojo, amarillo y verde)
+Haz los círculos del semáforo con HTML y CSS.
+
+Cuando un peatón quiere cruzar la calle debe pulsar un botón.
+Crea un botón para cambiar el color de las luces del semáforo
+secuencialmente (de rojo a verde, pasando por amarillo).
+
+Entonces,
+El semáforo siempre empieza en rojo.
+Cuando haces click en el botón, pasará a amarillo.
+Al hacer click otra vez, pasa a verde, y así todo el rato.
+*/
