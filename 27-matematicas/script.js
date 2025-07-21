99ed6d356441c1eb47c96a74a00d34e610086853
Nahuel C. <nahuelncejas@gmail.com>
2025-07-21 16:45:34 -0300

    objetos math

---
+++ 27-matematicas/script.js
@@ -0,0 +1,42 @@
+
+
+//?Objeto math
+//*Es tener una caja de herramientas listo para usarse, son metos estaticos y todos de matematicas
+
+//!Numero pi
+
+console.log(Math.PI)
+
+//!Numero Euler
+console.log(Math.E)
+
+//!Redondear
+
+//Redondear al entero mas cercano
+console.log(Math.round(7.6))
+
+//Redondear a la baja
+console.log(Math.floor(9.9))
+
+//Redondear a la alta
+console.log(Math.ceil(7.01))
+
+//!Numeros aleatorios
+
+console.log(Math.round(Math.random() *10))
+
+//!Valor absoluto
+console.log(Math.abs(-27.9))
+
+//!Potencias
+
+console.log(Math.pow(7,8))
+
+//!Raiz
+
+console.log(Math.sqrt(7).toFixed(2));
+
+//!Min y max
+
+console.log(Math.max(1, 7, 9, 29, 3))
+console.log(Math.min(1, 7, 9, 29, 3))
