a91958346dda41ae5307b3021e2b34fedfb90731
Nahuel C. <nahuelncejas@gmail.com>
2025-07-22 11:45:17 -0300

    Ejercicio

--- 29-ejercicios/script.js
+++ 29-ejercicios/script.js
@@ -1,3 +1,28 @@
+/*
+¡¡¡ SUPER EJERCICIO DE JAVASCRIPT !!!
+
+Crea una aplicación web en la que puedas añadir, mostrar y eliminar
+tus películas favoritas.
+
+Cada peli que guardes debe tener:
+
+- Título de la película  
+- Fecha de guardado  
+- Puntuación de popularidad (un valor aleatorio entre 1 y 100).
+
+Las películas deben mantenerse guardadas aunque se recargue la página,
+y se debe poder eliminar cualquiera de ellas de la lista en cualquier momento.
+
+Tareas:
+- Ejercicio 39: Crea el formulario necesario  
+- Ejercicio 40: Función para guardar películas con los datos necesarios  
+- Ejercicio 41: Función para mostrar películas extraídas del LocalStorage  
+- Ejercicio 42: Haz que las películas carguen automáticamente al abrir la web.  
+- Ejercicio 43: Función para eliminar películas.
+*/
+
+
+
 let formulario = document.querySelector("#formulario");
 let peliculaIngresada = document.querySelector("#pelicula");
 let cuerpoTabla = document.querySelector("#cuerpoTabla");
